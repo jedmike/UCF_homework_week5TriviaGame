eb04a78485a245535578eb73184819aab65652d6
Mike Jed <jedmike@gmail.com>
2019-10-19 08:57:09 -0400

    fixed .htmf file extension

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,10 @@
+//start game Trivia
+//start//set timeout timer, display
+//display questions
+    drfine object question, pro
+//display answers with radio buttons
+    
+//increment ccorrect counter (incorrect counter?)
+//check timeout timer, if zero display number of correcct answerss, otherwise wait
+    // turn wrong answers red, hilight correct answeers, 
+
